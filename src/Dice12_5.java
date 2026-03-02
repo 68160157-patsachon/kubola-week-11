888a4ad35d3bc3f96ecf716b91ccc10fdbf9038a
68160157 <68160157@go.buu.ac.th>
2026-03-02 23:39:40 +0700

    create LAB11

---
+++ src/Dice12_5.java
@@ -0,0 +1,12 @@
+
+public class Dice12_5 {
+    private int score;
+
+    public void roll() {
+        score = (int)(Math.random() * 6) + 1;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
